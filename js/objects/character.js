15ea421913a07ba1d02497065e93f30feaf37f96
Arnaud Huysman <Arnaud@MacBook-Pro-de-Arnaud.local>
2018-01-20 20:29:52 +0100

    Ajout page avec carte et mouvement du personnage.

--- js/objects/character.js
+++ js/objects/character.js
@@ -1,112 +1,224 @@
-CharCons = function(){
-  this.mesh = new THREE.Object3D();
-  this.mesh.name = "character";
-
-  //Body
-
-  var geomBody = new THREE.BoxGeometry(36, 30, 28, 1, 1, 1);
-  var matBody = new THREE.MeshPhongMaterial({color: Colors.blue, shading:THREE.FlatShading});
-
-  this.body = new THREE.Mesh(geomBody, matBody);
-  //body.castShadow = true;
-  this.body.receiveShadow = true;
-
-
-  // Head
-
-  var geomHead = new THREE.BoxGeometry(48, 48, 48, 1, 1, 1);
-  var matHead = new THREE.MeshPhongMaterial({color: 0xf25346, shading:THREE.FlatShading});
-
-  this.body.head = new THREE.Mesh(geomHead, matHead);
-  this.body.head.name = "head";
-  this.body.head.position.y = 40;
-  //head.castShadow = true;
-  this.body.head.receiveShadow = true;
-  this.body.add(this.body.head);
 
 
 
-  //Legs
+class Bodypart {
+  constructor(width, height, depth, color, name) {
 
-  var geomLeg = new THREE.BoxGeometry(16, 18, 18, 1, 1, 1);
-  var matLeg = new THREE.MeshPhongMaterial({color: Colors.orange, shading:THREE.FlatShading});
+    this.geom = new THREE.BoxGeometry(width, height, depth, 1, 1, 1);
+    this.mat = new THREE.MeshPhongMaterial({color, shading:THREE.FlatShading});
 
-  //Left Leg
-  this.body.leftLeg = new THREE.Mesh(geomLeg, matLeg);
+    this.mesh = new THREE.Mesh(this.geom, this.mat);
+    this.mesh.receiveShadow = true;
 
-  this.body.leftLeg.geometry.translate( 0, -9, 0 );
-  this.body.leftLeg.position.y = -18;
-  this.body.leftLeg.position.x = -9;
+    this.name = name;
 
-
-  //body.castShadow = true;
-  this.body.leftLeg.receiveShadow = true;
-  this.body.add(this.body.leftLeg);
-
-  //Right Leg
-  this.body.rightLeg = this.body.leftLeg.clone();
-
-  this.body.rightLeg.position.y = -18;
-  this.body.rightLeg.position.x = 9;
-  //rightLeg.rotation.x = Math.PI/8;
-  //body.castShadow = true;
-  this.body.rightLeg.receiveShadow = true;
-  this.body.add(this.body.rightLeg);
-
-  this.body.leftLeg.rotation.x = 0.5;
-  this.body.rightLeg.rotation.x = -0.5;
-
-  //Arms
-
-  var geomArm = new THREE.BoxGeometry(8, 24, 12, 1, 1, 1);
-  var matArm = new THREE.MeshPhongMaterial({color: Colors.orange, shading:THREE.FlatShading});
-
-  //Left Arm
-  this.body.leftArm = new THREE.Mesh(geomArm, matArm);
-
-  this.body.leftArm.geometry.translate(0,-12,0);
-  this.body.leftArm.position.y = 12;
-  this.body.leftArm.position.x = -24;
-
-
-  //body.castShadow = true;
-  this.body.leftArm.receiveShadow = true;
-  this.body.add(this.body.leftArm);
-
-  //Right Arm
-  this.body.rightArm = this.body.leftArm.clone();
-  this.body.rightArm.position.x = 24;
-  //body.castShadow = true;
-  this.body.rightArm.receiveShadow = true;
-  this.body.add(this.body.rightArm);
-
-  this.body.leftArm.rotation.x = -0.5;
-  this.body.rightArm.rotation.x = 0.5
-
-  this.mesh.add(this.body);
+  }
 }
+
+class Leg extends Bodypart {
+  constructor(width, height, depth, color, name) {
+
+    super(width, height, depth, color, name);
+
+    this.mesh.geometry.translate( 0, -9, 0 );
+    this.mesh.position.y = -18;
+    this.mesh.position.x = this.name == "leftLeg" ? -9 : 9;
+
+    this.mvt = this.name == "leftLeg" ? "backward" : "forward";
+    this.rot = this.name == "leftLeg" ? -0.05 : 0.05;
+
+  }
+
+  move(){
+
+    if(this.mesh.rotation.x > 0.4 && this.mvt == "forward")
+    {
+      this.rot = -0.05;
+      this.mvt ="backward"
+    } else if(this.mesh.rotation.x < -0.4 && this.mvt == "backward")
+    {
+      this.rot = 0.05;
+      this.mvt ="forward"
+    }
+
+    this.mesh.rotation.x += this.rot;
+  }
+}
+
+class Arm extends Bodypart {
+  constructor(width, height, depth, color, name) {
+
+      super(width, height, depth, color, name);
+
+      this.mesh.geometry.translate(0,-12,0);
+      this.mesh.position.y = 12;
+      this.mesh.position.x = this.name == "leftArm" ? -24 : 24;
+
+      this.mvt = this.name == "rightArm" ? "backward" : "forward";
+      this.rot = this.name == "rightArm" ? -0.05 : 0.05;
+  }
+
+  move(){
+
+    if(this.mesh.rotation.x > 0.4 && this.mvt == "forward")
+    {
+      this.rot = -0.05;
+      this.mvt ="backward"
+    } else if(this.mesh.rotation.x < -0.4 && this.mvt == "backward")
+    {
+      this.rot = 0.05;
+      this.mvt ="forward"
+    }
+
+    this.mesh.rotation.x += this.rot;
+
+  }
+}
+
+class Head extends Bodypart {
+  constructor(width, height, depth, color, name) {
+
+    super(width, height, depth, color, name);
+
+    this.mesh.position.y = 40;
+
+    this.rightEye = new Bodypart(8,8,8,0xFFFFFF, "rightEye");
+    this.rightEye.mesh.position.z += 23;
+    this.rightEye.mesh.position.y += 8;
+    this.rightEye.mesh.position.x += 10;
+
+    this.mesh.add(this.rightEye.mesh);
+
+    this.leftEye = new Bodypart(8,8,8,0xFFFFFFF, "leftEye");
+    this.leftEye.mesh.position.z += 23;
+    this.leftEye.mesh.position.y += 8;
+    this.leftEye.mesh.position.x += -10;
+
+    this.mesh.add(this.leftEye.mesh);
+
+
+    this.mvt = "up";
+    this.rot = 0.25;
+
+  }
+
+  move(){
+    if(this.mesh.position.y > 41 && this.mvt == "up")
+    {
+      this.rot = -0.25;
+      this.mvt ="down";
+    } else if(this.mesh.position.y < 39 && this.mvt == "down")
+    {
+      this.rot = 0.25;
+      this.mvt ="up";
+    }
+
+    this.mesh.position.y += this.rot;
+  }
+}
+
+
+// Body Class
+
+class Body extends Bodypart {
+  constructor(width, height, depth, color, name) {
+
+    super(width, height, depth, color, name);
+
+    this.movable = [];
+    // Head
+    this.head = new Head(48, 48, 48, 0xE53D00, "head");
+
+    this.mesh.add( this.head.mesh );
+    this.movable.push(this.head);
+
+    // Legs
+    this.leftLeg = new Leg(16, 18, 18, 0xE53D00, "leftLeg" );
+    this.rightLeg = new Leg(16, 18, 18, 0xE53D00, "rightLeg" );
+
+    this.mesh.add( this.leftLeg.mesh );
+    this.mesh.add( this.rightLeg.mesh );
+
+    this.movable.push(this.leftLeg,this.rightLeg);
+
+    // Arms
+    this.leftArm = new Arm(8, 24, 12, 0xE53D00, "leftArm" );
+    this.rightArm = new Arm(8, 24, 12, 0xE53D00, "rightArm" );
+
+    this.mesh.add( this.leftArm.mesh );
+    this.mesh.add( this.rightArm.mesh );
+
+    this.movable.push(this.leftArm,this.rightArm);
+
+
+  }
+
+  move(){
+
+    for (var i = 0; i < this.movable.length; i++) {
+      this.movable[i].move();
+    }
+
+  }
+}
+
+/*
+* Character Class
+*   - Body
+*   - Name
+*/
+
+class Char {
+
+  constructor(name){
+    this.mesh = new THREE.Object3D();
+    this.mesh.name = "character";
+    this.name = name;
+
+    this.state = "still";
+
+    this.body = new Body(36, 30, 28, 0x21A0A0);
+    this.body.mesh.position.y += 56;
+    this.mesh.add( this.body.mesh );
+
+  }
+
+  move(){
+    this.body.move();
+
+    switch (direction) {
+      case "left":
+        this.body.mesh.rotation.y = -Math.PI/2;
+        this.body.mesh.position.x -= 10;
+        break;
+      case "right":
+        this.body.mesh.rotation.y = Math.PI/2;
+        this.body.mesh.position.x += 10;
+        break;
+      case "up":
+        this.body.mesh.rotation.y = Math.PI;
+        this.body.mesh.position.z -= 10;
+        break;
+      case "down":
+        this.body.mesh.rotation.y = 0;
+        this.body.mesh.position.z += 10;
+        break;
+      default:
+
+    }
+  }
+}
+
 
 var char;
 
 function createCharacter(){
-  char = new CharCons();
+  char = new Char();
   scene.add(char.mesh);
 }
 
 function animateCharacter(body){
 
-  if(body.leftLeg.rotation.x > 0.4)
-  {
-    body.leftLeg.rotation.x -= 0.1;
-  } else if (body.leftLeg.rotation.x < -0.4)
-  {
-    body.leftLeg.rotation.x += 0.1;
-  } else {
-    body.leftLeg.rotation.x += 0.1;
-  }
-
-
-
-  body.rightLeg.rotation.x -= 0.1;
+  char.move();
 
 }
